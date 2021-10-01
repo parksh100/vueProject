226746f255b3f376a58b823efbe3a7083a4148a3
shpark <parksh1000@outlook.com>
2021-10-01 18:26:38 +0900

    first commit

---
+++ src/components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+  components: { Post },
+};
+</script>
+
+<style></style>
